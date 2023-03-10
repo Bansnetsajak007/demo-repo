1d82b97bf02b3b047d92e78148a9998994f8ec09
sajak <sajakbasnet77@gmail.com>
2023-03-10 20:41:31 +0545

    small change made

--- lower_to_upper.c
+++ lower_to_upper.c
@@ -11,7 +11,7 @@ int main() {
 
    // Convert string to uppercase
    for(i=0; str[i]!='\0'; i++) {
-      if(str[i]>='a' && str[i]<='z') {
+      if(str[i]>=97 && str[i]<=122) {
          str[i] = str[i] - 32;
       }
    }
@@ -19,7 +19,7 @@ int main() {
 
    // Convert string to lowercase
    for(i=0; str[i]!='\0'; i++) {
-      if(str[i]>='A' && str[i]<='Z') {
+      if(str[i]>=65 && str[i]<=90) {
          str[i] = str[i] + 32;
       }
    }
